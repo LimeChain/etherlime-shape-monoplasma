02d6dd92ef1542a691aa2b0e9770411721bc14f6
George Spasov <spasov05203@gmail.com>
2019-08-14 15:57:30 +0300

    Fixes on the readme and deplyoment

--- src/utils/deployDemoToken.js
+++ src/utils/deployDemoToken.js
@@ -1,11 +1,11 @@
 const TokenJson = require("../../build/DemoToken.json");
-const etherlime = require('etherlime');
 
-module.exports = async function deployDemoToken(provider,tokenName, tokenSymbol, sendOptions, privateKey, log) {
+module.exports = async function deployDemoToken(deployer, tokenName, tokenSymbol, sendOptions, privateKey, log) {
     log("Deploying a dummy token contract...")
     // const deployer = new etherlime.EtherlimeGanacheDeployer();
     // deployer.setPrivateKey(privateKey)
-    provider.setDefaultOverrides(sendOptions)
-    const result = await provider.deploy(TokenJson, {}, tokenName || "Demo token", tokenSymbol || "\ud83e\udd84");
+    deployer.setDefaultOverrides(sendOptions)
+    const result = await deployer.deploy(TokenJson, {}, tokenName || "Demo token", tokenSymbol || "\ud83e\udd84");
+    log(`Deployed at : ${result.contractAddress}`);
     return result.contractAddress;
 }
